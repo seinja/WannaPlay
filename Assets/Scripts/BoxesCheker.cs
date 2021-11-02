9bc0a2b1725b4885061499a7da1a6273aa259d62
Ilya <60188664+seinja@users.noreply.github.com>
2021-11-02 08:09:29 +0300

    Add cound down timer

--- Assets/Scripts/BoxesCheker.cs
+++ Assets/Scripts/BoxesCheker.cs
@@ -1,22 +1,38 @@
 ﻿using System.Collections;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class BoxesCheker : MonoBehaviour
 {
+    [SerializeField] private Slider coundDownSlider;
+
     [SerializeField] private Gun gun;
     [SerializeField] private GameObject rewindButton;
 
     private int countOfObjectsToRewind;
     private int countOfObjectsPrepare;
 
+    private bool isRewindNow;
+
     public void Start()
     {
+        InitCoundDownSlider();
+        
         rewindButton.SetActive(false);
         countOfObjectsToRewind = transform.hierarchyCount;
     }
 
+    public void FixedUpdate()
+    {
+        if (coundDownSlider.value > 0 && isRewindNow) 
+        {
+            coundDownSlider.value -= Time.fixedDeltaTime;
+        }
+    }
+
     public void StartRewindInChilds()
     {
+        coundDownSlider.value = transform.GetChild(0).GetComponent<TimeObject>().GetRecordTime;
         for (int i = 0; i < countOfObjectsToRewind - 1; i++)
         {
             transform.GetChild(i).GetComponent<TimeObject>().StartRewind();
@@ -30,10 +46,13 @@ public class BoxesCheker : MonoBehaviour
         rewindButton.SetActive(false);
         yield return new WaitForSeconds(transform.GetChild(0).GetComponent<TimeObject>().GetRecordTime);
         gun.ReloadGun();
+        isRewindNow = false;
+        coundDownSlider.value = coundDownSlider.maxValue;
     }
 
     public void StartCheckChild()
     {
+        
         for (int i = 0; i < countOfObjectsToRewind - 1; i++)
         {
             transform.GetChild(i).GetComponent<Box>().StartCoroutine(
@@ -55,9 +74,18 @@ public class BoxesCheker : MonoBehaviour
 
     public void StopCoroutineInChildrens()
     {
+        isRewindNow = true;
         for (int i = 0; i < countOfObjectsToRewind - 1; i++)
         {
             transform.GetChild(i).GetComponent<Box>().StopAllCoroutines();
         }
     }
+
+    private void InitCoundDownSlider() 
+    {
+        coundDownSlider.maxValue = transform.GetChild(0).GetComponent<TimeObject>().GetRecordTime;
+        coundDownSlider.value = coundDownSlider.maxValue;
+    }
+
+    
 }
